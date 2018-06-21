7e9ed8d255a54cd5a82d8b5df2f19fa0e974ad2e
Dzulfikri Pysal <32833301+Opapis@users.noreply.github.com>
2018-06-22 06:37:17 +0800

    Update dotsArray.js

--- js/dotsArray.js
+++ js/dotsArray.js
@@ -1,6 +1,6 @@
 colour = {"black": "#000", "red": "#ff0000", "yellow": "#ffff00", "blue":"#00406d"};
 lvlName = ["Stack A", "Stack B", "Stack C"];
-bgImg = ["image/stackA.jpg", "image/stackB.jpg", "image/stackC.jpg"];
+bgImg = ["image/stackA.jpg", "image/stackB.jpg", "image/stackC.jpg", "image/stackD.png", "image/stackE.png", "image/stackF.png", "image/stackG.png", "image/stackH.png"];
 allDots = [
     [   
        
@@ -227,8 +227,470 @@ allDots = [
             //dotnum: 13
         }
     ],
+	
 	[
-	]
+	//StackD
+        {
+            x: 79,
+            y: 45,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		{
+            x: 80,
+            y: 150,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 80,
+            y: 258,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 80,
+            y: 365,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 80,
+            y: 468,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 80,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 6
+        },
+		
+		{
+            x: 205,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 7
+        },
+		
+		{
+            x: 205,
+            y: 468,
+            colour: colour.black,
+            //dotnum: 8
+        },
+		
+		{
+            x: 340,
+            y: 467,
+            colour: colour.black,
+            //dotnum: 9
+        },
+		
+		{
+            x: 340,
+            y: 367,
+            colour: colour.black,
+            //dotnum: 10
+        },
+		
+		{
+            x: 445,
+            y: 365,
+            colour: colour.black,
+            //dotnum: 11
+        },
+		
+		{
+            x: 445,
+            y: 258,
+            colour: colour.black,
+            //dotnum: 12
+        },
+		
+		{
+            x: 570,
+            y: 258,
+            colour: colour.black,
+            //dotnum: 13
+        },
+		
+		{
+            x: 570,
+            y: 150,
+            colour: colour.black,
+            //dotnum: 14
+        },
+		
+		{
+            x: 720,
+            y: 150,
+            colour: colour.black,
+            //dotnum: 15
+        },
+		
+		{
+            x: 720,
+            y: 45,
+            colour: colour.black,
+            //dotnum: 15
+        }
+	],
+	
+	[
+	//StackE
+        {
+            x: 165,
+            y: 465,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		{
+            x: 258,
+            y: 465,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 258,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 165,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 258,
+            y: 365,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 350,
+            y: 365,
+            colour: colour.black,
+            //dotnum: 6
+        },
+				
+		{
+            x: 350,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 7
+        },
+		
+		{
+            x: 350,
+            y: 280,
+            colour: colour.black,
+            //dotnum: 8
+        },
+		
+		{
+            x: 443,
+            y: 280,
+            colour: colour.black,
+            //dotnum: 9
+        },
+		
+		{
+            x: 443,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 10
+        },
+		
+		{
+            x: 443,
+            y: 190,
+            colour: colour.black,
+            //dotnum: 11
+        },
+		
+		{
+            x: 535,
+            y: 190,
+            colour: colour.black,
+            //dotnum: 12
+        },
+		
+		{
+            x: 535,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 13
+        },
+		
+		{
+            x: 535,
+            y: 75,
+            colour: colour.black,
+            //dotnum: 14
+        },
+		
+		{
+            x: 630,
+            y: 75,
+            colour: colour.black,
+            //dotnum: 15
+        },
+		
+		{
+            x: 630,
+            y: 560,
+            colour: colour.black,
+            //dotnum: 15
+        }
+		
+		
+		
+		
+	],
+		[   
+		//StackF
+        {
+            x: 133,
+            y: 45,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		 {
+            x: 238,
+            y: 45,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 238,
+            y: 170,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 238,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 133,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 343,
+            y: 170,
+            colour: colour.black,
+            //dotnum: 6
+        },
+		
+		
+		{
+            x: 343,
+            y: 275,
+            colour: colour.black,
+            //dotnum: 7
+        },
+		
+		{
+            x: 343,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 8
+        },
+		
+		{
+            x: 448,
+            y: 275,
+            colour: colour.black,
+            //dotnum: 9
+        },
+		
+		{
+            x: 448,
+            y: 360,
+            colour: colour.black,
+            //dotnum: 10
+        },
+		
+		{
+            x: 448,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 11
+        },
+		
+		{
+            x: 553,
+            y: 360,
+            colour: colour.black,
+            //dotnum: 12
+        },
+		
+		
+		{
+            x: 553,
+            y: 475,
+            colour: colour.black,
+            //dotnum: 13
+        },
+		
+		{
+            x: 553,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 14
+        },
+		
+		{
+            x: 657,
+            y: 475,
+            colour: colour.black,
+            //dotnum: 15
+        },
+		
+		{
+            x: 657,
+            y: 575,
+            colour: colour.black,
+            //dotnum: 15
+        }
+		
+	],
+	
+	[//StackG
+        {
+            x: 165,
+            y: 85,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		{
+            x: 323,
+            y: 85,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 478,
+            y: 85,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 633,
+            y: 85,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 79,
+            y: 487,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 242,
+            y: 487,
+            colour: colour.black,
+            //dotnum: 6
+        },
+		
+		{
+            x: 396,
+            y: 487,
+            colour: colour.black,
+            //dotnum: 7
+        },
+		
+		{
+            x: 553,
+            y: 488,
+            colour: colour.black,
+            //dotnum: 8
+        },
+		
+		{
+            x: 712,
+            y: 488,
+            colour: colour.black,
+            //dotnum: 9
+        }
+	],
+	
+	[ //stackH
+	
+	//StackH
+        {
+            x: 108,
+            y: 285,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		{
+            x: 235,
+            y: 120,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 409,
+            y: 75,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 580,
+            y: 130,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 699,
+            y: 285,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 406,
+            y: 485,
+            colour: colour.black,
+            //dotnum: 6
+        }
+	]	
+	
+	
 ];
 
 
