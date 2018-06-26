6ec1697738a864fa7764dda7650d17a7e3b861cc
Dzulfikri Pysal <32833301+Opapis@users.noreply.github.com>
2018-06-26 13:04:05 +0800

    Add files via upload

---
+++ asd/js/dotsArray.js
@@ -0,0 +1,89 @@
+colour = {"black": "#000", "red": "#ff0000", "yellow": "#ffff00", "blue":"#00406d"};
+lvlName = ["Stack A", "Stack B", "Stack C"];
+bgImg = ["image/stackG.PNG", "image/stackB.jpg", "image/stackC.jpg"];
+allDots = [
+    [   
+       
+		// buat code untuk dots kau kat sini
+		// nanti cut je prgi main code yang aq bagi sebelum ni
+		//StackG
+        {
+            x: 147,
+            y: 168,
+            colour: colour.black,
+            //dotnum: 1
+        },
+		
+		{
+            x: 318,
+            y: 168,
+            colour: colour.black,
+            //dotnum: 2
+        },
+		
+		{
+            x: 486,
+            y: 168,
+            colour: colour.black,
+            //dotnum: 3
+        },
+		
+		{
+            x: 654,
+            y: 168,
+            colour: colour.black,
+            //dotnum: 4
+        },
+		
+		{
+            x: 60,
+            y: 394,
+            colour: colour.black,
+            //dotnum: 5
+        },
+		
+		{
+            x: 230,
+            y: 394,
+            colour: colour.black,
+            //dotnum: 6
+        },
+		
+		{
+            x: 400,
+            y: 394,
+            colour: colour.black,
+            //dotnum: 7
+        },
+		
+		{
+            x: 570,
+            y: 394,
+            colour: colour.black,
+            //dotnum: 8
+        },
+		
+		{
+            x: 732,
+            y: 394,
+            colour: colour.black,
+            //dotnum: 9
+        }
+	],
+	
+	
+	
+		
+	
+    [ 
+       
+    ],
+    [ 
+      
+    ],
+	[
+	]
+];
+
+
+		
